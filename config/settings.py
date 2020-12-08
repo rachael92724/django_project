5d5268dd898440d7bf63a4c282dc15c60c44b877
rachael92724 <rachael927@naver.com>
2020-12-08 23:49:56 +0900

    DJango Project

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'o0bvp9i1xmvm&z3ixqiar80n&c7t^=5$1!%%mfwwbzs9p@ablx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = '*'
 
 
 # Application definition
