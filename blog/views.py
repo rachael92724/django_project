bf5769419ebcbf1eb87e83c7ea580b750fb55bab
rachael92724 <rachael927@naver.com>
2020-12-21 21:32:41 +0900

    Django Project

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.views.generic.edit import DeleteView
 from .models import Post
 from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
-    posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
 
 def post_detail(request, pk):
@@ -42,3 +43,8 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
 
     return render(request, 'blog/post_edit.html', {'form':form})
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('/blog')
